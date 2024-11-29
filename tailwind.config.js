91406f03333d28389d06d715cca69b980e12fee7
JonathanRobinsonAristizabalSoto <jonathan_aristizabal@soy.sena.edu.co>
2024-11-29 16:22:50 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,200 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        color1: '#ffffff',       // Blanco
+        color2: '#cccccc',       // Gris claro
+        color3: '#696969',       // Gris
+        color4: '#413f4c',       // Gris oscuro
+        color5: '#00ab00',       // Verde
+        color6: '#ff893a',       // Naranja
+      },
+      screens: {
+        'xs': '320px',    // Móvil más pequeño
+        'sm': '640px',    // Móvil
+        'md': '768px',    // Tablet
+        'lg': '1024px',   // Laptop
+        'xl': '1280px',   // Desktop
+        'tv': '1920px',   // TV
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      fontWeight: {
+        'thin': 100,
+        'extralight': 200,
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
+      },
+      fontSize: {
+        'xs': '0.75rem', // 12px
+        'sm': '0.875rem', // 14px
+        'base': '1rem', // 16px
+        'lg': '1.125rem', // 18px
+        'xl': '1.25rem', // 20px
+        '2xl': '1.375rem', // 22px
+        '3xl': '1.5rem', // 24px
+        '4xl': '1.75rem', // 28px
+        '5xl': '2rem', // 32px
+        '6xl': '2.25rem', // 36px
+        '7xl': '2.5rem', // 40px
+        '8xl': '3rem', // 48px
+        '9xl': '3.75rem', // 60px
+      },
+      width: {
+        'auto': 'auto',
+        'px': '1px',
+        '0': '0px',
+        '0.5': '0.125rem', // 2px
+        '1': '0.25rem', // 4px
+        '1.5': '0.375rem', // 6px
+        '2': '0.5rem', // 8px
+        '2.5': '0.625rem', // 10px
+        '3': '0.75rem', // 12px
+        '3.5': '0.875rem', // 14px
+        '4': '1rem', // 16px
+        '5': '1.25rem', // 20px
+        '6': '1.5rem', // 24px
+        '7': '1.75rem', // 28px
+        '8': '2rem', // 32px
+        '9': '2.25rem', // 36px
+        '10': '2.5rem', // 40px
+        '11': '2.75rem', // 44px
+        '12': '3rem', // 48px
+        '14': '3.5rem', // 56px
+        '16': '4rem', // 64px
+        '20': '5rem', // 80px
+        '24': '6rem', // 96px
+        '28': '7rem', // 112px
+        '32': '8rem', // 128px
+        '36': '9rem', // 144px
+        '40': '10rem', // 160px
+        '44': '11rem', // 176px
+        '48': '12rem', // 192px
+        '52': '13rem', // 208px
+        '56': '14rem', // 224px
+        '60': '15rem', // 240px
+        '64': '16rem', // 256px
+        '72': '18rem', // 288px
+        '76': '19rem', // 304px
+        '80': '20rem', // 320px
+        '96': '24rem', // 384px
+        '112': '28rem', // 448px
+        '128': '32rem', // 512px
+        '144': '36rem', // 576px
+        '160': '40rem', // 640px
+        '176': '44rem', // 704px
+        '192': '48rem', // 768px
+        '208': '52rem', // 832px
+        '224': '56rem', // 896px
+        '240': '60rem', // 960px
+        'full': '100%',
+        'screen': '100vw',
+        'min': 'min-content',
+        'max': 'max-content',
+      },
+      height: {
+        'auto': 'auto',
+        'px': '1px',
+        '0': '0px',
+        '0.5': '0.125rem', // 2px
+        '1': '0.25rem', // 4px
+        '1.5': '0.375rem', // 6px
+        '2': '0.5rem', // 8px
+        '2.5': '0.625rem', // 10px
+        '3': '0.75rem', // 12px
+        '3.5': '0.875rem', // 14px
+        '4': '1rem', // 16px
+        '5': '1.25rem', // 20px
+        '6': '1.5rem', // 24px
+        '7': '1.75rem', // 28px
+        '8': '2rem', // 32px
+        '9': '2.25rem', // 36px
+        '10': '2.5rem', // 40px
+        '11': '2.75rem', // 44px
+        '12': '3rem', // 48px
+        '14': '3.5rem', // 56px
+        '16': '4rem', // 64px
+        '20': '5rem', // 80px
+        '24': '6rem', // 96px
+        '28': '7rem', // 112px
+        '32': '8rem', // 128px
+        '36': '9rem', // 144px
+        '40': '10rem', // 160px
+        '44': '11rem', // 176px
+        '48': '12rem', // 192px
+        '52': '13rem', // 208px
+        '56': '14rem', // 224px
+        '60': '15rem', // 240px
+        '64': '16rem', // 256px
+        '72': '18rem', // 288px
+        '76': '19rem', // 304px
+        '80': '20rem', // 320px
+        '96': '24rem', // 384px
+        '112': '28rem', // 448px
+        '128': '32rem', // 512px
+        '144': '36rem', // 576px
+        '160': '40rem', // 640px
+        '176': '44rem', // 704px
+        '192': '48rem', // 768px
+        '208': '52rem', // 832px
+        '224': '56rem', // 896px
+        '240': '60rem', // 960px
+        'full': '100%',
+        'screen': '100vh',
+        'min': 'min-content',
+        'max': 'max-content',
+      },
+      margin: {
+        'auto': 'auto',
+        'px': '1px',
+        '0': '0px',
+        '0.5': '0.125rem', // 2px
+        '1': '0.25rem', // 4px
+        '1.5': '0.375rem', // 6px
+        '2': '0.5rem', // 8px
+        '2.5': '0.625rem', // 10px
+        '3': '0.75rem', // 12px
+        '3.5': '0.875rem', // 14px
+        '4': '1rem', // 16px
+        '5': '1.25rem', // 20px
+        '6': '1.5rem', // 24px
+        '7': '1.75rem', // 28px
+        '8': '2rem', // 32px
+        '9': '2.25rem', // 36px
+        '10': '2.5rem', // 40px
+        '11': '2.75rem', // 44px
+        '12': '3rem', // 48px
+        '14': '3.5rem', // 56px
+        '16': '4rem', // 64px
+        '20': '5rem', // 80px
+        '24': '6rem', // 96px
+        '28': '7rem', // 112px
+        '32': '8rem', // 128px
+        '36': '9rem', // 144px
+        '40': '10rem', // 160px
+        '44': '11rem', // 176px
+        '48': '12rem', // 192px
+        '52': '13rem', // 208px
+        '56': '14rem', // 224px
+        '60': '15rem', // 240px
+        '64': '16rem', // 256px
+        '72': '18rem', // 288px
+        '76': '19rem', // 304px
+        '80': '20rem', // 320px
+        '96': '24rem', // 384px
+      },
+    },
+  },
+  plugins: [],
+}
